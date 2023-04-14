ecdc9c8080a414cc277659b7ea0b04233611ae3a
8169009963 <nandiaditi093@gmail.com>
2023-04-14 18:11:02 +0530

    project

---
+++ main.py
@@ -0,0 +1,43 @@
+from dash import Dash, html, dcc, Input, Output
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('C:/Users/ADMIN/Desktop/avocado-updated-2020.csv')
+
+df.info()
+
+print(df['type'].value_counts(dropna=False))
+print(df['geography'].value_counts(dropna=False))
+
+msk = df['geography'] == 'Los Angeles'
+
+fig = px.line(df[msk], x='date', y='average_price', color='type')
+
+fig.show()
+
+avocado = pd.read_csv('C:/Users/ADMIN/Desktop/avocado-updated-2020.csv')
+
+
+app = Dash()
+
+geo_dropdown = dcc.Dropdown(options=avocado['geography'].unique(),
+                            value='New York')
+
+app.layout = html.Div(children=[
+    html.H1(children='Avocado Prices Dashboard'),
+    geo_dropdown,
+    dcc.Graph(id='price-graph')
+])
+@app.callback(
+    Output(component_id='price-graph', component_property='figure'),
+    Input(component_id=geo_dropdown, component_property='value')
+)
+def update_graph(selected_geography):
+    filtered_avocado = avocado[avocado['geography'] == selected_geography]
+    line_fig = px.line(filtered_avocado,
+                       x='date', y='average_price',
+                       color='type',
+                       title=f'Avocado Prices in {selected_geography}')
+    return line_fig
+if __name__ == '__main__':
+    app.run_server(debug=True)
